36e2a04fc9ab72eda0b7829b5ee5a3162c2385ec
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-20 15:56:37 +1100

    feat (backend): Added playlist functionality to the converter

--- backend/services/converter.py
+++ backend/services/converter.py
@@ -8,8 +8,11 @@ def run_converter(youtube_url, output_folder="downloads"):
     # Configuration ofor yt_dlp
     ydl_opts = {
         'format': 'bestaudio/best',
-        # Naming: This saves it as 'Title.mp3' inside the downloads folder
-        'outtmpl': f'{output_folder}/%(title)s.%(ext)s',
+        # If part of a playlist, put it in a subfolder. If not, just the downloads folder.
+        'outtmpl': {
+            'default': f'{output_folder}/%(title)s.%(ext)s',
+            'playlist': f'{output_folder}/%(playlist_title)s/%(title)s.%(ext)s'
+        },
         
         # This is the "Magic" part that triggers the conversion
         'postprocessors': [{
@@ -19,6 +22,10 @@ def run_converter(youtube_url, output_folder="downloads"):
         }],
         
         # WSL specific: ensures it finds FFmpeg
+        'noplaylist': False,        # Allow playlists
+        'extract_flat': False,      # Ensure it actually downloads the audio, not just metadata
+        'playlist_items': '1-20',   # SAFETY: Limit to first 20 songs so your PC doesn't explode
+
         'prefer_ffmpeg': True,
         'keepvideo': False,  # Deletes the original video/m4a after mp3 is made
     }
